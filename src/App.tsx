e88556c5ea8e1a47a0dfab877a7e59ed953679ce
xsahxl <xsahxl@126.com>
2021-03-08 21:05:34 +0800

    add ui find

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import './App.css'
-import { Button } from '@alicloud/console-components'
+import React, { useEffect } from 'react'
+import { Table, Field } from '@alicloud/console-components'
+import axios from 'axios'
+import styled from 'styled-components'
+import { get } from 'lodash'
 
 function App() {
+  const field = Field.useField()
+  const { setValue, getValue } = field
+
+  const fetchData = async () => {
+    const res = await axios.get('/api/user')
+    setValue('data', get(res, 'data.data'))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <h1>Console Components Demo</h1>
-      <Button type="primary">Button works!</Button>
-    </div>
+    <SWrapper>
+      <Table dataSource={getValue('data')}>
+        <Table.Column title="姓名" dataIndex="name" />
+        <Table.Column title="年龄" dataIndex="age" />
+      </Table>
+    </SWrapper>
   )
 }
+
+const SWrapper = styled.div`
+  padding: 32px;
+`
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import '@alicloud/console-components/dist/wind.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
