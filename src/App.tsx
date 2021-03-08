1bf2d9a3768094a8e9e8bc9ede7743774661d500
xsahxl <xsahxl@126.com>
2021-03-08 19:49:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import { Button } from '@alicloud/console-components'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Console Components Demo</h1>
+      <Button type="primary">Button works!</Button>
+    </div>
+  )
+}
+
+export default App
